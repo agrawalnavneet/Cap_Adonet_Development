5353579a9e962e15308b83a3e5011d0bfe78f264
Navneet Agarwal <agrawalnavneet960@gmail.com>
2026-02-28 15:55:55 +0530

    adedd

---
+++ collage form/collage form/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using collage_form.Models;
+
+namespace collage_form.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ collage form/collage form/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace collage_form.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z\s]+$",
+            ErrorMessage = "Name cannot contain numbers")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(17, 100,
+            ErrorMessage = "Age must be greater than 16")]
+        public int Age { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z\s]+$",
+            ErrorMessage = "Father name cannot contain numbers")]
+        public string FatherName { get; set; } = string.Empty;
+
+        [Required]
+        public string Place { get; set; } = string.Empty;
+    }
+}
