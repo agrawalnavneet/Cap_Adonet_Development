8e096ce39d072aa7ddcfc90f757677ee2b3cd837
Navneet Agarwal <agrawalnavneet960@gmail.com>
2026-03-09 12:02:23 +0530

    added

---
+++ Day47/app/Models/Order.cs
@@ -0,0 +1,19 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace app.Models{
+public class Order
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string ProductName { get; set; }
+
+    public decimal Amount { get; set; }
+
+    // Foreign key
+    public int CustomerId { get; set; }
+
+    // Navigation property
+    public Customer Customer { get; set; }
+}}
