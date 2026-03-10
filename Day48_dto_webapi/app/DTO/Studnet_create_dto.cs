eb4a39a38dd3a187967d8867cacdb44963433dcd
Navneet Agarwal <agrawalnavneet960@gmail.com>
2026-03-10 10:50:35 +0530

    added swagger

---
+++ Day48_dto_webapi/app/DTO/Studnet_create_dto.cs
@@ -0,0 +1,11 @@
+namespace app.DTO
+{
+    public class StudentCreateRequestDTO
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public decimal CourseFeePaid { get; set; }
+    }
+}
---
+++ Day48_dto_webapi/app/Models/Student.cs
@@ -0,0 +1,13 @@
+namespace app.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public decimal CourseFeePaid { get; set; }
+    }
+}
